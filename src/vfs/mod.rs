7c6f18ee7e257d6c0fd05bf53318650df9eec020
kcza <ed@kcza.net>
2026-02-05 17:15:00 +0000

    Add `#[must_use]` annotation to `VfsRegistrationGuard`

--- src/vfs/mod.rs
+++ src/vfs/mod.rs
@@ -938,6 +938,7 @@ pub enum AtomicWrite {
 }
 
 /// RAII guard that unregisters a VFS on drop.
+#[must_use]
 pub struct VfsRegistrationGuard<V>(Arc<VfsStorage<V>>);
 
 impl<V> Deref for VfsRegistrationGuard<V> {
